28f0ecbc3f0d6a3c5a056668958fab50f8baba75
t <t>
2020-08-19 16:20:46 +0100

    function for wrangling the separate an and my data and joining together tidied up a bit

--- bc-analysis.py
+++ bc-analysis.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import itertools as it
-from plotnine import *
-
+import plotnine as p9
 #import data
 # turn this into a loop or function sometime
 andata = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/covstatstotal.txt", sep=' ', names=["num_baits", "sample", "depth"]) 
@@ -12,23 +11,19 @@ anmeta = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/meta
 
 #get sampleset identifiers (in this case the number of samples in the set)
 #and then get fraction of baits captured to our degree of coverage as a fraction of the total baits for each sampleset
-mycolval = pd.Series([len(mymeta.index) for i in range(len(mydata.index))])
-mydata.insert(loc = 0, column = 'ninds', value = mycolval)
-mydata['frac'] = mydata['num_baits']/24444
-#repeat
-ancolval = pd.Series([len(anmeta.index) for i in range(len(andata.index))])
-andata.insert(loc = 0, column = 'ninds', value = ancolval)
-andata['frac'] = andata['num_baits']/148729
+def wrangle(datafile, metadatafile, numbaits): 
+    colval = pd.Series([len(metadatafile.index) for i in range(len(datafile.index))])
+    datafile.insert(loc = 0, column = 'ninds', value = colval)
+    datafile['frac'] = datafile['num_baits']/numbaits
+    return(datafile)
+    
 #bind together
-df = pd.concat([andata, mydata])
+df = pd.concat([wrangle(mydata, mymeta, 24444), wrangle(andata, anmeta, 148729)])
+
 df['organism'] = np.where(df['ninds']==113, 'anthrax', 'mycoplasma')
-
-
-
 
 #intialise empty dataframe
 size_df = pd.DataFrame(columns=["frac", "coverage", "ninds", "size"])
-
 
 #we want to calculate the number of individuals where we covered the genome to a depth of 0X to 400X, and a breadth of 0-1 (as fraction)
 #so we create the cartesian product of these ranges, expand them into a triplet in a for loop and iterate over them all (3-way nested loop)
@@ -40,32 +35,27 @@ for f, c, i in it.product(np.arange(0.1,1.0,0.1), np.arange(0,410,10),[113, 56])
     
 #get the fraction of individuals per sample set by dividing the number of individuals matching each condition
 size_df['frac_inds'] = size_df['size']/size_df['ninds']
-
-
-test = size_df[(size_df.ninds == 56)]
-
+#add organism name for ninds as above (boring and redundant)
 size_df['organism'] = np.where(size_df['ninds']==113, 'anthrax', 'mycoplasma')
-
-
 
 #plot depth of coverage to which an increasing fraction of baits are totally covered in both
 #sample sets
 
 (
- ggplot(size_df, aes(x = 'coverage', y = 'frac_inds', colour = 'organism'))+
-  facet_wrap('frac') +
-  geom_line() +
-  xlab("Depth of Coverage") +
-  ylab("Fraction of Individuals") +
-  theme_minimal()
+ p9.ggplot(size_df, p9.aes(x = 'coverage', y = 'frac_inds', colour = 'organism'))+
+  p9.facet_wrap('frac') +
+  p9.geom_line() +
+  p9.xlab("Depth of Coverage") +
+  p9.ylab("Fraction of Individuals") +
+  p9.theme_minimal()
  )
 
 #plot fraction of baits covered to x depth in all individuals for both anth and myco
 (
- ggplot(df, aes(x='depth', y ='frac', colour = 'sample')) +
-     geom_line() +
-     facet_wrap('organism') +
-     scale_color_discrete(guide=False) +
-     theme_minimal() 
+ p9.ggplot(df, p9.aes(x='depth', y ='frac', colour = 'sample')) +
+     p9.geom_line() +
+     p9.facet_wrap('organism') +
+     p9.scale_color_discrete(guide=False) +
+     p9.theme_minimal() 
  )
 
