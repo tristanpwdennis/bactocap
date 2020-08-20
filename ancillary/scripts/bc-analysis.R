4ff9b3896dc85d8d951390777f5741190547094e
t <t>
2020-08-20 12:43:01 +0100

    replace bcanalysis.R with updated plotting

--- ancillary/scripts/bc-analysis.R
+++ ancillary/scripts/bc-analysis.R
@@ -1,22 +1,20 @@
 #####examining coverage over bait sequences for mycoplasma and anthrax
 
-library(dplyr)
-library(ggplot2)
 library(viridis)
 library(data.table)
+library(tidyverse)
+library(GGally)
 
 #get metadata for mycoplasma and anthrax
-mycodata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/mycoplasma/metadata/myco-seq-sample-data.csv")
-anthdata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/metadata/anthrax-metadata-1.csv")
+mycodata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/mycoplasma/metadata/myco-seq-sample-data.csv", na.strings = c("", "NA"))
+anthdata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/metadata/anthrax-metadata-1.csv",na.strings = c("", "NA"))
 
 #remove trailing underscore
 #mycoseqdata$sample_id <- sub("_[^_]+$", "", mycoseqdata$sample_id)
 
 #load paths for coverage info
-anthraxpath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/covstats.txt")
-mycopath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/mycoplasma/covstats.txt")
-anthraxhi <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/200to400covstats.txt")
-mycohi <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/mycoplasma/filtered_200to400covstats.txt")
+anthraxpath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/0to400covstats.txt")
+mycopath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/mycoplasma/covstatstotal.txt")
 
 #function to get our files and derive the fraction of bait captures for each coverage level
 get_bait_cap_frac <- function(fp, numbaits) {
@@ -26,81 +24,43 @@ get_bait_cap_frac <- function(fp, numbaits) {
   return(cov2)
 }
 
-an <- bind_rows(get_bait_cap_frac(anthraxpath, 148729), get_bait_cap_frac(anthraxhi, 148729))
-my <- bind_rows(get_bait_cap_frac(mycopath, 24444), get_bait_cap_frac(mycohi, 24444))
+an <- get_bait_cap_frac(anthraxpath, 148729)
+my <- get_bait_cap_frac(mycopath, 24444)
 
+#plot fraction
 ggplot(my, aes(x = coverage, y = frac, color = sampleid)) +
   geom_line(show.legend = FALSE) +
   theme(legend.position = "none") +
   theme_minimal() 
 
-
-
-
-#call function and concatenate results for both anthrax and mycoplasma into single df
-concatenated_frac_cov_ind <- rbind(get_noinds_cov(an, anthdata, 'anthrax'), get_noinds_cov(my, mycodata, 'mycoplasma'))
-
-#weird result for 200 as I split the calc over two windows - will rerun and plot again
-t<-filter(concatenated_frac_cov_ind, capture_rate != 200)
-
-#plot
-ggplot(t, aes(x = capture_rate, y = prop_ind, color = organism)) +
-  geom_line()+
-  xlab("Depth of coverage over 90% of the captured baits") + ylab("Fraction of Samples") +
-  theme_minimal() 
-
-
-
+#ok now we combine into one big dataframe
 my$organism <- paste0('mycoplasma')
 my$ninds <- nrow(mycodata)
 an$organism <- paste0('anthrax')
 an$ninds <- nrow(anthdata)
 
-comb <- rbind(an, my)
+#join anthrax bait frac data with metadata, clean some dud values, change percentages to decimals
+bcdata <- left_join(an, anthdata, by = c('sampleid' = 'sample_id')) %>% filter(coverage == 30) %>% select(sampleid, highest_ct, captured_lib_conc, reads, frac_mapped_reads, frac)
+bcdata$frac_mapped_reads <- as.numeric(sub("%", "",bcdata$frac_mapped_reads,fixed=TRUE))/100
+bcdata <- bcdata %>% na_if('x') %>% na_if('ND')
 
-#find the cartesian product of the coverage and genome fraction ranges (every % genome covered at every d.o.c), and organism
-covfrac <- expand.grid(seq(10,400, 10), seq (0.01, 1, 0.01), as.numeric(c('56', '117')))
+#plot covariance
+bcdata %>% 
+  select(frac, highest_ct, captured_lib_conc, reads, frac_mapped_reads) %>% 
+  sample_frac(0.5, replace = TRUE) %>% 
+  ggpairs(axislabels = "none") +
+  theme_bw()
 
-testfunc <- function(se, fracse, numin) {
-  x <- nrow(filter(comb, coverage == se & frac > fracse & ninds == numin))/numin
-}
+mcdata <- left_join(my, mycodata, by = c('sampleid' = 'ID_on_remote')) %>% filter(coverage == 30) %>% select(sampleid, highest.Ct, captured.lib.conc, reads, Percentage.mapped.reads, frac)
+bcdata$Percentage.mapped.reads <- as.numeric(sub("%", "",bcdata$Percentage.mapped.reads,fixed=TRUE))/100
+bcdata <- bcdata %>% na_if('x') %>% na_if('ND')
 
-results <- as.data.frame(mapply(testfunc, covfrac[,1], covfrac[2], covfrac[,3]))
-t <- cbind(results, covfrac)
+mycodata$frac = mycodata$baits_covered_.30/24444
+mycodata$sample_id
+my$sampleid <-sub("_[^_]+$", "", my$sampleid)
+mycodata$frac_mapped_reads = mycodata$mapped/mycodata$total
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#percent of samples, per anthrax and mycoplasma, covered to each X
-
-get_noinds_cov <- function(df, metadata, orgname) {
-  prop <- data.frame()
-  for (i in seq(10,400, 10)) {
-    x <- nrow(filter(df, coverage == i & frac > 0.9))/nrow(metadata)
-    prop = rbind(prop, x)
-  }
-  prop <- cbind(prop, as.data.frame(seq(10,400, 10)))
-  colnames(prop)<-c('prop_ind', 'capture_rate')
-  prop$organism <- paste0(orgname)
-  return(prop)
-}
-
+mycodata %>% select(total, mapped, captured_amamplified_library_conc_ngul, udg_PCR_run1, frac) %>% 
+  sample_frac(0.5, replace = TRUE) %>% 
+  ggpairs(axislabels = "none") +
+  theme_bw()
