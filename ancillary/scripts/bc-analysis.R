3b9ca7a77948f3cb92e4db9820577b53c234f570
t <t>
2020-09-11 09:59:46 +0100

    mv base scripts to dedicated dir in ancillary merge scripts into rmd

--- ancillary/scripts/bc-analysis.R
+++ ancillary/scripts/bc-analysis.R
@@ -1,66 +1,38 @@
 #####examining coverage over bait sequences for mycoplasma and anthrax
-
 library(viridis)
-library(data.table)
 library(tidyverse)
 library(GGally)
-
+library(reticulate)
 #get metadata for mycoplasma and anthrax
 mycodata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/mycoplasma/metadata/myco-seq-sample-data.csv", na.strings = c("", "NA"))
 anthdata <- read.csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/metadata/anthrax-metadata-1.csv",na.strings = c("", "NA"))
 
-#remove trailing underscore
-#mycoseqdata$sample_id <- sub("_[^_]+$", "", mycoseqdata$sample_id)
 
-#load paths for coverage info
-anthraxpath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/0to400covstats.txt")
-mycopath <- c("/Users/tristanpwdennis/Projects/bactocap/datasets/mycoplasma/covstatstotal.txt")
 
-#function to get our files and derive the fraction of bait captures for each coverage level
-get_bait_cap_frac <- function(fp, numbaits) {
-  cov <- read.csv(fp, sep  = ' ', header = F)
-  colnames(cov) <- c('no_baits', 'sampleid', 'coverage')
-  cov2 <- mutate(cov, frac = (numbaits - no_baits)/numbaits*100)
-  return(cov2)
-}
-
-an <- get_bait_cap_frac(anthraxpath, 148729)
-my <- get_bait_cap_frac(mycopath, 24444)
-
-#plot fraction
-ggplot(my, aes(x = coverage, y = frac, color = sampleid)) +
-  geom_line(show.legend = FALSE) +
-  theme(legend.position = "none") +
-  theme_minimal() 
-
-#ok now we combine into one big dataframe
-my$organism <- paste0('mycoplasma')
-my$ninds <- nrow(mycodata)
-an$organism <- paste0('anthrax')
-an$ninds <- nrow(anthdata)
-
-#join anthrax bait frac data with metadata, clean some dud values, change percentages to decimals
-bcdata <- left_join(an, anthdata, by = c('sampleid' = 'sample_id')) %>% filter(coverage == 30) %>% select(sampleid, highest_ct, captured_lib_conc, reads, frac_mapped_reads, frac)
-bcdata$frac_mapped_reads <- as.numeric(sub("%", "",bcdata$frac_mapped_reads,fixed=TRUE))/100
-bcdata <- bcdata %>% na_if('x') %>% na_if('ND')
 
 #plot covariance
 bcdata %>% 
-  select(frac, highest_ct, captured_lib_conc, reads, frac_mapped_reads) %>% 
+  select(total_reads, highest_ct, captured_lib_conc, rate_mapped_reads, pcnt_30X_captures,bc_camp_cycles, lp_amp_cycles ) %>% 
   sample_frac(0.5, replace = TRUE) %>% 
   ggpairs(axislabels = "none") +
   theme_bw()
 
-mcdata <- left_join(my, mycodata, by = c('sampleid' = 'ID_on_remote')) %>% filter(coverage == 30) %>% select(sampleid, highest.Ct, captured.lib.conc, reads, Percentage.mapped.reads, frac)
-bcdata$Percentage.mapped.reads <- as.numeric(sub("%", "",bcdata$Percentage.mapped.reads,fixed=TRUE))/100
-bcdata <- bcdata %>% na_if('x') %>% na_if('ND')
-
-mycodata$frac = mycodata$baits_covered_.30/24444
-mycodata$sample_id
+mycodata$frac = (24444 - mycodata$baits_covered_.30)/24444
 my$sampleid <-sub("_[^_]+$", "", my$sampleid)
 mycodata$frac_mapped_reads = mycodata$mapped/mycodata$total
+mycodat <- mycodata %>% select(total, frac_mapped_reads, captured_amamplified_library_conc_ngul, udg_PCR_run1, frac)
+colnames(mycodat) <- c('total_reads', 'rate_mapped_reads', 'captured_lib_conc', 'highest_ct', 'pcnt_30X_captures')
 
-mycodata %>% select(total, mapped, captured_amamplified_library_conc_ngul, udg_PCR_run1, frac) %>% 
+
+mycodat %>% select(total_reads, highest_ct, captured_lib_conc, rate_mapped_reads, pcnt_30X_captures) %>% 
   sample_frac(0.5, replace = TRUE) %>% 
   ggpairs(axislabels = "none") +
   theme_bw()
+
+
+  
+summary(mycodata$frac_mapped_reads)
+summary(mycodata$dupfrac)
+
+
+
--- ancillary/scripts/bc-analysis.py
+++ ancillary/scripts/bc-analysis.py
@@ -4,7 +4,7 @@ import itertools as it
 import plotnine as p9
 #import data
 # turn this into a loop or function sometime
-andata = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/covstatstotal.txt", sep=' ', names=["num_baits", "sample", "depth"]) 
+andata = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap/datasets/anthrax/results/0to400covstats.txt", sep=' ', names=["num_baits", "sample", "depth"]) 
 mydata = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap/datasets/mycoplasma/covstatstotal.txt", sep=' ', names=["num_baits", "sample", "depth"])
 mymeta = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap-data/mycoplasma/metadata/myco-seq-sample-data.csv")
 anmeta = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/metadata/anthrax-metadata-1.csv")
@@ -14,7 +14,7 @@ anmeta = pd.read_csv("/Users/tristanpwdennis/Projects/bactocap-data/anthrax/meta
 def wrangle(datafile, metadatafile, numbaits): 
     colval = pd.Series([len(metadatafile.index) for i in range(len(datafile.index))])
     datafile.insert(loc = 0, column = 'ninds', value = colval)
-    datafile['frac'] = datafile['num_baits']/numbaits
+    datafile['frac'] = (numbaits - (datafile['num_baits']))/numbaits
     datafile['organism'] = np.where(datafile['ninds']==113, 'anthrax', 'mycoplasma')
     return(datafile)
     
@@ -31,8 +31,10 @@ for f, c, i in it.product(np.arange(0.1,1.0,0.1), np.arange(0,410,10),[113, 56])
     size = mask.sum()
     size_df.loc[len(size_df)] = [f, c, i, size]
     
+
 #get the fraction of individuals per sample set by dividing the number of individuals matching each condition
 size_df['frac_inds'] = size_df['size']/size_df['ninds']
+
 
 #add organism name for ninds as above (boring and redundant)
 size_df['organism'] = np.where(size_df['ninds']==113, 'anthrax', 'mycoplasma')
@@ -40,14 +42,25 @@ size_df['organism'] = np.where(size_df['ninds']==113, 'anthrax', 'mycoplasma')
 #plot depth of coverage to which an increasing fraction of baits are totally covered in both
 #sample sets
 
+
+
+size_df['frac'] = size_df['frac']*100
+
+
 (
  p9.ggplot(size_df, p9.aes(x = 'coverage', y = 'frac_inds', colour = 'organism'))+
-  p9.facet_wrap('frac') +
   p9.geom_line() +
+  p9.facet_wrap('frac') +
   p9.xlab("Depth of Coverage") +
-  p9.ylab("Fraction of Individuals") +
-  p9.theme_minimal()
+  p9.ylab("Fraction of Sample Set") +
+  p9.theme_bw()
  )
+
+
+
+
+
+
 
 #plot fraction of baits covered to x depth in all individuals for both anth and myco
 (
@@ -55,6 +68,10 @@ size_df['organism'] = np.where(size_df['ninds']==113, 'anthrax', 'mycoplasma')
      p9.geom_line() +
      p9.facet_wrap('organism') +
      p9.scale_color_discrete(guide=False) +
-     p9.theme_minimal() 
+     p9.xlab("Depth of Coverage") +
+     p9.ylab("Fraction of Baits Captured") +
+     p9.theme_bw() 
  )
 
+df[(df.frac > 0.75) & (df.depth > 300)]
+df[(df['sample'] == 'AN17-187_S19')]
