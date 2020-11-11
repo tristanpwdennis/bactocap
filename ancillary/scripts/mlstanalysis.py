4f624a6b794380ffefc3cef02f01639af85a8b8c
t <t>
2020-11-11 15:01:54 +0000

    collect mapping stats per-target per-sample with pysam and pandas collect per-position depth per-target per-sample with pybedtools and pandas

--- ancillary/scripts/mlstanalysis.py
+++ ancillary/scripts/mlstanalysis.py
@@ -7,21 +7,41 @@ Created on Thu Nov  5 11:34:37 2020
 """
 
 import os
-import numpy as np
 import pandas as pd
 import pysam 
+import pybedtools as pbt
 from io import StringIO
 
 bamdir = "/Users/tristanpwdennis/Projects/bactocap/datasets/mlst/results/bam"
 
-li = []
-
+#collect mapping data from bam file indexes
+#using pysam to collect mapping stats per chromosome
+#for each bam file, calculate coverage over each chromosome position - creating a df each time that is appended to 'cov'
+#concatenate into one big dataframe with 'sample_id', ''target', 'position', 'depth'
+idx = []
 for entry in os.scandir(bamdir):
     if(entry.path.endswith(".bam")):
        bamtab = StringIO(pysam.idxstats(entry.path))
        bamtab = pd.read_table(bamtab, names = ['locus', 'sequence_length', 'mapped_reads', 'unmapped_reads'])
        bannamestring = os.path.splitext(os.path.basename(entry.path))[0]
-       bamtab.insert(0, 'bamname', bannamestring)
-       li.append(bamtab)
+       bamtab.insert(0, 'sample_id', bannamestring)
+       idx.append(bamtab)
        
-idxstats = pd.concat(li, axis=0, ignore_index=True)
+#collect list of dataframes into a single dataframe       
+idxstats = pd.concat(idx, axis=0, ignore_index=True)
+
+#collect coverage data from bam files
+#for each bam file, calculate coverage over each chromosome position - creating a df each time that is appended to 'cov'
+#concatenate into one big dataframe with 'sample_id', ''target', 'position', 'depth'
+cov = []
+for entry in os.scandir(bamdir):
+    if(entry.path.endswith(".bam")):
+        bamobj = pbt.BedTool(entry.path)
+        covstats = bamobj.genome_coverage(d=True, **{'5': True})
+        t = pd.read_table(covstats.fn, names=['chrom', 'pos', 'dp'])
+        bannamestring = os.path.splitext(os.path.basename(entry.path))[0]
+        t.insert(0, 'sample_id', bannamestring)
+        cov.append(t)
+        
+covs =  pd.concat(cov, axis=0, ignore_index=True)      
+
